09cb55989eba89ebb20a2766992b873af2b46561
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-12-01 16:35:56 +0100

    upload

--- index.php
+++ index.php
@@ -2,15 +2,26 @@
 //votre formulaire  ici!
   require 'header.php';
   include './vues/formulaire.php';
-  require "./src/dao/RestaurantRepository.php"; // Inclure le fichier du repository
+  require "./src/dao/CandidateRepository.php"; // Inclure le fichier du repository
+
   $repo = new CandidateRepository(); //Créer une instance du repository
 
-var_export($repo->infoTable());
+  
   ?>
   <body>
     
+<?php
+$password = "mot_de_passe_utilisateur123";
+$hashedPassword = encryptPassword($password);
+// Stockez $hashedPassword dans votre base de données
 
+// Plus tard, pour vérifier :
+if (verifyPassword($password, $hashedPassword)) {
+    echo "Le mot de passe est correct !";
+} else {
+    echo "Mot de passe invalide.";
+}
 
-
+?>
 </body>
 </html>
--- src/dao/CandidateRepository.php
+++ src/dao/CandidateRepository.php
@@ -3,12 +3,53 @@ require_once __DIR__ . '/Dbconnexion.php';
 
 class CandidateRepository
 {
-    private PDO $pdo; // Instance de PDO pour la connexion à la base de données
-    private string $table = "candidats"; // Nom de la table
+    private PDO $pdo;
+    private string $table = "candidats";
 
-    public function __construct() // Constructeur
+    public function __construct()
     {
-        $this->pdo = Dbconnexion::getInstance(); // Récupère l'instance de PDO
+        $this->pdo = Dbconnexion::getInstance();
     }
+
+    public function searchAll(): array
+    {
+        $sql = "SELECT lastname_user, firstname, mail_user, pass_user, departement_user, age_user FROM {$this->table}";
+        $stmt = $this->pdo->query($sql);
+        return $stmt->fetchAll();
+    }
+
+    private function encryptPassword(string $password): string {
+        return password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    public function createCandidate(string $lastname, string $firstname, int $age, string $email, string $password): bool {
+
+
+        $hashedPassword = $this->encryptPassword($password);
+
+        $sql = "INSERT INTO {$this->table} (lastname_user, firstname, age_user, mail_user, pass_user, departement_user) 
+                VALUES (:lastname, :firstname, :age, :email, :pass, :departement)";
+
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            
+
+            $stmt->bindParam(':lastname', $lastname);
+            $stmt->bindParam(':firstname', $firstname);
+            $stmt->bindParam(':age', $age, PDO::PARAM_INT);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':pass', $hashedPassword);
+            $stmt->bindParam(':departement', $departement);
+
+            return $stmt->execute();
+        } catch (PDOException $e) {
+            echo "Erreur lors de l'insertion : " . $e->getMessage();
+            return false;
+        }
+    }
+
+    function verifyPassword(string $password, string $hash): bool {
+    return password_verify($password, $hash);
+}
 
 }
